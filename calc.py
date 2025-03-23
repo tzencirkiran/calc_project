3c2e1b2b3e388dc04f8292d2b904bf14705a7262
talha <t.zencirkiran35@gmail.com>
2025-03-23 15:39:49 +0300

    Add division features

--- calc.py
+++ calc.py
@@ -8,9 +8,13 @@ def sub(x,y):
 def mod(x,y):
     return x % y
 
+def div(x,y):
+    return x/y if y!=0 else "can't divide by zero"
+
 if __name__ == "__main__":
     print(add(10,20))
     print(sub(10,20))
     print(mod(10,20))
+    print(div(10,2))
 
     
