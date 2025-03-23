d68d24d5bed049c607ae8473d76b6858a1046201
talha <t.zencirkiran35@gmail.com>
2025-03-23 15:33:32 +0300

    add modules features

--- calc.py
+++ calc.py
@@ -5,8 +5,12 @@ def add(x,y):
 def sub(x,y):
     return x - y
 
+def mod(x,y):
+    return x % y
+
 if __name__ == "__main__":
     print(add(10,20))
     print(sub(10,20))
+    print(mod(10,20))
 
     
