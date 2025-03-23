6b783788f336c591131006e4e0195dd4e9cd470e
talha <t.zencirkiran35@gmail.com>
2025-03-23 15:30:47 +0300

    add multiplication features

--- calc.py
+++ calc.py
@@ -5,8 +5,12 @@ def add(x,y):
 def sub(x,y):
     return x - y
 
+
+def mul(x,y):
+    return x * y
+
 if __name__ == "__main__":
     print(add(10,20))
     print(sub(10,20))
+    print(mul(10,20))
 
-    
